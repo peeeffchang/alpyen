07248353d38372d1a34eb8cbd746b7689556299a
peeeffchang <peeeffchang@gmail.com>
2021-10-04 19:37:51 +0800

    minor change

--- tests/test_alpyne.py
+++ tests/test_alpyne.py
@@ -43,19 +43,23 @@ def test_backtesting_macrossing():
     # Read data
     data_folder_path = 'Data\\'
     ticker_name = 'BBH'
+    short_lookback = 5
+    long_lookback = 200
     ticker_names = [ticker_name]
     all_input = datacontainer.DataUtils.aggregate_yahoo_data(ticker_names, data_folder_path)
 
     # Subscribe to signals
     signal_info_dict = {}
-    signal_info_dict[ticker_name + '_MA_5'] = backtesting.SignalInfo(ticker_names, [5])
-    signal_info_dict[ticker_name + '_MA_200'] = backtesting.SignalInfo(ticker_names, [200])
+    signal_info_dict[ticker_name + '_MA_' + str(short_lookback)]\
+        = backtesting.SignalInfo(ticker_names, [short_lookback])
+    signal_info_dict[ticker_name + '_MA_' + str(long_lookback)]\
+        = backtesting.SignalInfo(ticker_names, [long_lookback])
 
     # Subscribe to strategies
     strategy_info_dict = {}
     strategy_name = ticker_name + '_MACrossing_01'
     strategy_info_dict[strategy_name] = backtesting.StrategyInfo(
-        [ticker_name + '_MA_5', ticker_name + '_MA_200'], [1], [ticker_name])
+        [ticker_name + '_MA_' + str(short_lookback), ticker_name + '_MA_' + str(long_lookback)], [1], [ticker_name])
 
     # Create backtester
     number_path = 1000
