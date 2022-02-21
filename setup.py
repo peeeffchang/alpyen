df95c2999e56da43e27398dcea907bb62657027c
peeeffchang <peeeffchang@gmail.com>
2022-02-21 16:40:16 +0800

    versioning

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     },
     install_requires=requirements,
     license="GNU General Public License v3",
+    long_description_content_type="text/markdown",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='alpyen',
@@ -48,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/peeeffchang/alpyen',
-    version='1.0.1',
+    version='1.0.2',
     zip_safe=False,
 )
