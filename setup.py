5cccdeabb4de3b6e92b3b288d9cab503204f58e7
peeeffchang <peeeffchang@gmail.com>
2022-03-08 11:10:29 +0800

    alpyen and numpy versioning

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/peeeffchang/alpyen',
-    version='1.0.2',
+    version='1.1.0',
     zip_safe=False,
 )
