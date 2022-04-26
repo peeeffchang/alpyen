ca6bf48d009fac13060874ff3a2eb0d16a2c2660
peeeffchang <peeeffchang@gmail.com>
2022-04-26 10:39:47 +0800

    Adding new dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,16 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'ib-insync', 'eventkit', 'pandas', 'asyncio', 'arch', 'numpy' ]
+requirements = [
+    'Click>=7.0', 
+    'ib-insync', 
+    'eventkit', 
+    'pandas', 
+    'asyncio', 
+    'arch', 
+    'numpy' , 
+    'gemini_python'
+]
 
 test_requirements = ['pytest>=3', ]
 
